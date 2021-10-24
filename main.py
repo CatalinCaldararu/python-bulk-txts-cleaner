f051309b555c58c813d675b348e287492ed75efd
myusername <catalin.caldararu99@gmail.com>
2021-10-24 14:16:03 +0300

    Implemented the recursive article upload function

--- main.py
+++ main.py
@@ -2,28 +2,41 @@ from selenium import webdriver
 import os
 directory = os.getcwd()
 
-#Open the Chrome Instance
+#Step 0, Preparing the upload function
+def recursiveTxtUpload():
+    for r, _, f in os.walk(r'articles'):
+        for _f in f:
+            apath = os.path.join(r, _f)
+            _, ext = os.path.splitext(apath)
+            if ext == '.txt':
+                try:
+                    browser.get("http://localhost:8002/wordpress/wp-admin/post-new.php")
+                    text_area = browser.find_element_by_css_selector("textarea[id='post-title-0']")
+                    with open(apath) as text:
+                        words = text.read().split()
+                        for word in words:
+                         text_area.send_keys(word)
+                         text_area.send_keys(" ")
+                except Exception as e:
+                    print(f'Error while processing {apath} -> {e}')
+
+#Step 1, The setup and the initial number of articles that we want to uplpad
+file_count = sum(len(files) for _, _, files in os.walk(r'articles'))
+print(file_count)
 browser = webdriver.Chrome()
 
-#Loop --> Iterate through the txt files and publish them to wordpress
-#Step 1 Connect To Wordpress
-browser.get("http://localhost:8002/wordpress/wp-admin/post-new.php")
+#Step 2, The initial login to Wordpress
+
+browser.get("http://localhost:8002/wordpress/wp-admin/")
 username_field = browser.find_element_by_id("user_login")
 password_field = browser.find_element_by_id("user_pass")
 button_login = browser.find_element_by_css_selector("input[type='submit']")
-
-#Step 2 Login to WordPress
 username_field.send_keys("user")
 password_field.send_keys("#CatalinCaldararu#")
 button_login.click()
 
-#Step 3 Define your TXT contents
-hello_variable = "Hello world"
+#Step 3, Publishing the articles
+directory = 'articles'
+recursiveTxtUpload()
 
-#Step 4 Copy your TXT contents to the textarea
-text_area = browser.find_element_by_css_selector("textarea[id='post-title-0']")
-text_area.send_keys(hello_variable)
 
-#Step 5 Press the publish button
-
-#Repeat Until Completed
