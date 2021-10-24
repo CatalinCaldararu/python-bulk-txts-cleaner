53186d9cb2771d065c291007066c7611afbfb119
myusername <catalin.caldararu99@gmail.com>
2021-10-24 15:24:39 +0300

    Implemented the function for article deletion

---
+++ recursiveDeleteArticles.py
@@ -0,0 +1,19 @@
+import os
+directory = os.getcwd()
+
+def recursiveDeleteArticles(wordLimit):
+    for r, _, f in os.walk(r'C:\Users\HP\Desktop\Articles'):
+        for _f in f:
+            apath = os.path.join(r, _f)
+            _, ext = os.path.splitext(apath)
+            if ext == '.txt':
+                try:
+                    rflag = False
+                    with open(apath) as text:
+                        if len(text.read().split()) < wordLimit:
+                            rflag = True
+                    if rflag:
+                        os.remove(apath)
+                        print(f'{apath} was deleted')
+                except Exception as e:
+                    print(f'Error while processing {apath} -> {e}')
