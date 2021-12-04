9e1e50e32ac1b3bc386ce620d668e3ac1b7cff98
myusername <catalin.caldararu99@gmail.com>
2021-12-04 20:07:51 +0200

    Refactor project structure

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import options as options
 from selenium import webdriver
-from articleUpload import recursiveArticleUpload
-from wordpressLogin import loginToWordPress
+from Utils.wordpressLogin import loginToWordPress
 
-options = webdriver.ChromeOptions()
-options.add_argument(r'--disable-popup-blocking')
-browser=webdriver.Chrome(chrome_options=options, executable_path=r"C:/Users/HP/projects/drivers/chromedriver.exe")
-
-#browser = webdriver.Chrome()
+browser = webdriver.Chrome()
 loginToWordPress(browser)
-recursiveArticleUpload(browser)
